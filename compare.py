557b7299c155e01b157692f1537fdf063c266e6d
Bloom <tiewyinger@gmail.com>
2025-05-25 15:04:58 +0800

    testing

---
+++ compare.py
@@ -0,0 +1,16 @@
+import os
+import json
+import requests
+import traceback
+from flask import Blueprint, request, session, render_template, flash, redirect, url_for
+from dotenv import load_dotenv
+
+load
+
+
+@app.route('/compare_candidates', methods=['GET'])
+def compare_candidates():
+
+
+
+
